a2c5d7bdf6e85d49167c1b9c9fa924289cbc86a4
Sergio Tz <sergiotz@gmail.com>
2021-08-21 20:20:03 -0300

    initial version

---
+++ src/main/java/com/sergiotz/backend/BackendTodoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sergiotz.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTodoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTodoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergiotz/backend/BackendTodoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiotz.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTodoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
